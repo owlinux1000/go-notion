f74014ff3567581af2e2ea374bcd8a9162accccd
Chihiro Hasegawa <chihiro-h@mercari.com>
2025-10-07 18:35:58 +0900

    Delete omitempty so that you can send a request with an empty name

--- database.go
+++ database.go
@@ -92,7 +92,7 @@ const (
 
 type SelectOptions struct {
 	ID    string `json:"id,omitempty"`
-	Name  string `json:"name,omitempty"`
+	Name  string `json:"name"`
 	Color Color  `json:"color,omitempty"`
 }
 
